66d54335442cc38967adcde0e4ed9a9a7e780603
Sander <sanderdsz@gmail.com>
2022-11-03 18:15:25 -0300

    refactor: main app cleanup

--- src/main/java/com/sanderdsz/grocery/GroceryApplication.java
+++ src/main/java/com/sanderdsz/grocery/GroceryApplication.java
@@ -2,11 +2,6 @@ package com.sanderdsz.grocery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-
 
 @SpringBootApplication
 public class GroceryApplication {
