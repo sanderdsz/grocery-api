f3f15290cb2fa9a853737d5bb9d8cd0932d34d79
Sander <sanderdsz@gmail.com>
2022-11-03 18:16:26 -0300

    chore: UserController added

---
+++ src/main/java/com/sanderdsz/grocery/application/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.sanderdsz.grocery.application.controller;
+
+import com.sanderdsz.grocery.domain.model.User;
+import com.sanderdsz.grocery.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> list() {
+        return userService.listAll();
+    }
+
+    @PostMapping
+    public User save(
+            @RequestBody
+            User user
+    ) {
+        return userService.save(user);
+    }
+
+}
