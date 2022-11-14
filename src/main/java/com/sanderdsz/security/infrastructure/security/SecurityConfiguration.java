68a5b2b171bf5851f3d5b05167ed193ba1f1afc2
Sander <sanderdsz@gmail.com>
2022-11-14 14:46:13 -0300

    chore: added cors permission

--- src/main/java/com/sanderdsz/security/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/sanderdsz/security/infrastructure/security/SecurityConfiguration.java
@@ -1,19 +1,17 @@
-package com.sanderdsz.grocery.infrastructure.security;
+package com.sanderdsz.security.infrastructure.security;
 
-import com.sanderdsz.grocery.infrastructure.service.UserService;
+import com.sanderdsz.security.infrastructure.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.session.SessionManagementFilter;
 
 /**
  * The SecurityConfiguration class configures the routing of the API.
@@ -48,15 +46,24 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    CorsFilter corsFilter() {
+        CorsFilter filter = new CorsFilter();
+        return filter;
+    }
+
+    @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.disable())
+                .addFilterBefore(corsFilter(), SessionManagementFilter.class)
                 .authorizeRequests(auth -> {
                     auth.antMatchers("/auth/signup").permitAll();
                     auth.antMatchers("/auth/login").permitAll();
                     auth.antMatchers("/auth/logout").permitAll();
                     auth.antMatchers("/auth/alive").authenticated();
+                    auth.antMatchers("/recovery").permitAll();
+                    auth.antMatchers("/users/me").authenticated();
                 })
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .addFilterAfter(accessTokenFilter(), UsernamePasswordAuthenticationFilter.class)
